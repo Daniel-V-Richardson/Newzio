bfa3747fa80d323ff35b398a9e5d66b1a5bc5128
Daniel-V-Richardson <richardson20june@gmail.com>
2022-11-16 12:26:28 +0530

    Updated

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 import requests
-from sqlalchemy import case
 
 app = Flask(__name__)
 app.secret_key = "123456789"
@@ -9,16 +8,60 @@ app.secret_key = "123456789"
 # https://newsapi.org/v2/top-headlines?country=in&category=general&apikey=04936e1d20cf4a7db857af7d646cb0a7
 @app.route('/')
 def index():
-    url = "https://newsapi.org/v2/top-headlines?country=in&category=general&apikey=04936e1d20cf4a7db857af7d646cb0a7"
-    r = requests.get(url).json()
+    
+    url = "https://newsapi.org/v2/top-headlines?country=in&category=general&language=en&apikey=04936e1d20cf4a7db857af7d646cb0a7"
+    top = "https://newsapi.org/v2/top-headlines?sources=techcrunch&language=en&apiKey=04936e1d20cf4a7db857af7d646cb0a7"
+    
+    url_request = requests.get(url).json()
+    top_request= requests.get(top).json()
+    
     case = {
-        'articles': r['articles']
+        'articles': url_request['articles']
     }
-    return render_template('news.html', cases = case)
+    top ={
+       'articles': top_request['articles']
+    }
+   
+    return render_template('index.html', cases = case, tops = top)
+    
+# Routing for Top Cities:
+#Chennai
+@app.route('/chennai')
+def chennai():
+    top = "https://newsapi.org/v2/top-headlines?sources=techcrunch&language=en&apiKey=04936e1d20cf4a7db857af7d646cb0a7"
+    chennai ="https://newsapi.org/v2/everything?q=chennai&language=en&sortBy=publishedAt&apiKey=04936e1d20cf4a7db857af7d646cb0a7"
+    chennai_request= requests.get(chennai).json()
+    top_request= requests.get(top).json()
+    top ={
+       'articles': top_request['articles']
+    }
+    
+    chennai ={
+        'articles': chennai_request['articles']
+    }
+    return render_template('chennai.html',tops = top ,news = chennai)
+
+# Bengaluru
+@app.route('/bangalore')
+def bangalore():
+    top = "https://newsapi.org/v2/top-headlines?sources=techcrunch&language=en&apiKey=04936e1d20cf4a7db857af7d646cb0a7"
+    chennai ="https://newsapi.org/v2/everything?q=bangalore&language=en&sortBy=publishedAt&apiKey=04936e1d20cf4a7db857af7d646cb0a7"
+    chennai_request= requests.get(chennai).json()
+    top_request= requests.get(top).json()
+    top ={
+       'articles': top_request['articles']
+    }
+    
+    chennai ={
+        'articles': chennai_request['articles']
+    }
+    return render_template('bangalore.html',tops = top ,news = chennai)
+
 
 @app.route('/tech')
 def tech():
     url= "https://newsapi.org/v2/top-headlines?category=technology&apikey=04936e1d20cf4a7db857af7d646cb0a7"
+
     r = requests.get(url).json()
     headline = {
         'articles': r['articles']
